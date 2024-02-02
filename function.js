64d3fcd84322834a6e1ac804a8a4d52a3af7f370
devmahin <mahinhowlader123@gmail.com>
2024-02-02 21:31:46 +0600

    return

--- function.js
+++ function.js
@@ -1,10 +1,79 @@
-function paramiter(num1, num2){
-    let par = num1 * num2;
-    // let par2 = num1 + num2;
-    // console.log(parseFloat(par2))
-    console.log(par)
+// function paramiter(num1, num2){
+//     let par = num1 * num2;
+//     // let par2 = num1 + num2;
+//     // console.log(parseFloat(par2))
+//     console.log(par)
 
+// }
+// let argument = 50;
+// let argument2 = 10;
+// paramiter(argument, argument2)
+
+
+
+
+// return
+// function argument(par1, par2){
+//     let multiplication = par1 * par2;
+//     return multiplication;
+    
+// }
+// let store = argument(20,30)
+// console.log(store)
+
+
+
+// function doMath(par1, par2){
+//     let addition = par1 + par2;
+//     let substraction= par1 - par2;
+//     let divition = addition / substraction;
+//     // console.log(divition)
+//     let mult = divition * substraction
+//     return mult;
+// }
+// let result = doMath(30,20)
+// console.log(result)
+
+
+
+
+// even Number return
+// function even (a){
+//     if(a % 2 === 0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+// let evenNum = even(10)
+// console.log(evenNum)
+
+
+
+function even (a){
+    if(a % 2 === 0){
+        return true
+    }
+        return false
 }
-let argument = 50;
-let argument2 = 10;
-paramiter(argument, argument2)
+// let evenNum = even(10)
+// let evenNum2 = even(11)
+// console.log(evenNum)
+// console.log(evenNum2)
+
+
+
+
+// odd Number
+function even (a){
+    if(a % 2 === 1){
+        return true
+    }
+        return false
+}
+
+let evenNum = even(10)
+let evenNum2 = even(11)
+console.log(evenNum)
+console.log(evenNum2)
