9ba8ac9e3234d06cc1563a9d3365415bd57a1120
devmahin <mahinhowlader123@gmail.com>
2024-02-02 20:49:27 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,10 @@
+function paramiter(num1, num2){
+    let par = num1 * num2;
+    // let par2 = num1 + num2;
+    // console.log(parseFloat(par2))
+    console.log(par)
+
+}
+let argument = 50;
+let argument2 = 10;
+paramiter(argument, argument2)
